adbee990972e8ed8f5e1535e919c97900b97805c
Pavel Koshkin <koshkin.pasha@gmail.com>
2018-10-20 12:41:52 +0300

    first OK

--- extention/get_urls.js
+++ extention/get_urls.js
@@ -7,7 +7,6 @@ function getLastTS() {
     xhr.open("POST", url, true);
 
     xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
 
     xhr.onreadystatechange = function () {
         if (xhr.readyState === 4 && xhr.status === 200) {
